d0ea6d6ef75933a35e7ed4b8ca150a6847bc6aef
Abdullah Safdar <sheikhsafdar381@gmail.com>
2025-06-20 10:29:03 +0500

    Fix: updated MongoDB URI and connection in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (err) {
     console.error(err.message);
